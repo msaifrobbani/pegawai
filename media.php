830239a81de48364baf7e1edd98b02695a540f76
msaifrobbani <saif.robbani04@gmail.com>
2019-01-08 15:25:54 +0700

    baru lagi...

--- media.php
+++ media.php
@@ -81,26 +81,131 @@ if ($_SESSION['leveluser'] == '1' or $_SESSION['leveluser'] == '2') {
 	<?php include "data.php"; ?>  
         <?php 
         include_once "./config/koneksi.php";
+        // arsiparis aktif
+        $arsip1 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.1' AND id_statkerja='1'");
+        $ar1 = mysqli_fetch_array($arsip1);
 
-        $sql = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.3' AND id_statkerja='1'");
-        $jafung = mysqli_fetch_array($sql);
+        $arsip2 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.2' AND id_statkerja='1'");
+        $ar2 = mysqli_fetch_array($arsip2);
 
-        $sql1 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.4' AND id_statkerja='1'");
-        $jafung1 = mysqli_fetch_array($sql1);
+        $arsip3 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.3' AND id_statkerja='1'");
+        $ar3 = mysqli_fetch_array($arsip3);
 
-        $sql2 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.5' AND id_statkerja='1'");
-        $jafung2 = mysqli_fetch_array($sql2);
+        $arsip4 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.4' AND id_statkerja='1'");
+        $ar4 = mysqli_fetch_array($arsip4);
 
-        $sql3 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.3' AND id_statkerja='2'");
-        $bebas = mysqli_fetch_array($sql3);
+        $arsip5 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.5' AND id_statkerja='1'");
+        $ar5 = mysqli_fetch_array($arsip5);
 
-        $sql4 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.4' AND id_statkerja='2'");
-        $bebas1 = mysqli_fetch_array($sql4);
+        $arsip6 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.6' AND id_statkerja='1'");
+        $ar6 = mysqli_fetch_array($arsip6);
 
-        $sql5 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.5' AND id_statkerja='2'");
-        $bebas2 = mysqli_fetch_array($sql5);
+        $arsip7 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.7' AND id_statkerja='1'");
+        $ar7 = mysqli_fetch_array($arsip7);
 
+        // arsiparis bebas
+        $arsip8 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.1' AND id_statkerja='2'");
+        $arb1 = mysqli_fetch_array($arsip8);
 
+        $arsip9 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.2' AND id_statkerja='2'");
+        $arb2 = mysqli_fetch_array($arsip9);
+
+        $arsip10 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.3' AND id_statkerja='2'");
+        $arb3 = mysqli_fetch_array($arsip10);
+
+        $arsip11 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.4' AND id_statkerja='2'");
+        $arb4 = mysqli_fetch_array($arsip11);
+
+        $arsip12 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.5' AND id_statkerja='2'");
+        $arb5 = mysqli_fetch_array($arsip12);
+
+        $arsip13 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.6' AND id_statkerja='2'");
+        $arb6 = mysqli_fetch_array($arsip13);
+
+        $arsip14 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='06.7' AND id_statkerja='2'");
+        $arb7 = mysqli_fetch_array($arsip14);
+
+        //analis kepegawaian aktif
+        $analis1 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.1' AND id_statkerja='1'");
+        $ankep1 = mysqli_fetch_array($analis1);
+
+        $analis2 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.2' AND id_statkerja='1'");
+        $ankep2 = mysqli_fetch_array($analis2);
+
+        $analis3 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.3' AND id_statkerja='1'");
+        $ankep3 = mysqli_fetch_array($analis3);
+
+        $analis4 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.4' AND id_statkerja='1'");
+        $ankep4 = mysqli_fetch_array($analis4);
+
+        $analis5 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.5' AND id_statkerja='1'");
+        $ankep5 = mysqli_fetch_array($analis5);
+
+        $analis6 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.6' AND id_statkerja='1'");
+        $ankep6 = mysqli_fetch_array($analis6);
+
+        //analis kepegawaian bebas
+        $analis7 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.1' AND id_statkerja='2'");
+        $bkep1 = mysqli_fetch_array($analis7);
+
+        $analis8 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.2' AND id_statkerja='2'");
+        $bkep2 = mysqli_fetch_array($analis8);
+
+        $analis9 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.3' AND id_statkerja='2'");
+        $bkep3 = mysqli_fetch_array($analis9);
+
+        $analis10 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.4' AND id_statkerja='2'");
+        $bkep4 = mysqli_fetch_array($analis10);
+
+        $analis11 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.5' AND id_statkerja='2'");
+        $bkep5 = mysqli_fetch_array($analis11);
+
+        $analis12 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='07.6' AND id_statkerja='2'");
+        $bkep6 = mysqli_fetch_array($analis12);
+
+        //auditor pelaksana aktif
+        $audit1 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.1' AND id_statkerja='1'");
+        $akdit1 = mysqli_fetch_array($audit1);
+
+        $audit2 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.2' AND id_statkerja='1'");
+        $akdit2 = mysqli_fetch_array($audit2);
+
+        $audit3 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.3' AND id_statkerja='1'");
+        $akdit3 = mysqli_fetch_array($audit3);
+
+        $audit4 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.4' AND id_statkerja='1'");
+        $akdit4 = mysqli_fetch_array($audit4);
+
+        $audit5 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.5' AND id_statkerja='1'");
+        $akdit5 = mysqli_fetch_array($audit5);
+
+        $audit6 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.6' AND id_statkerja='1'");
+        $akdit6 = mysqli_fetch_array($audit6);
+
+        $audit7 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.7' AND id_statkerja='1'");
+        $akdit7 = mysqli_fetch_array($audit7);
+
+        //auditor pelaksana bebas
+        $audit8 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.1' AND id_statkerja='2'");
+        $bedit1 = mysqli_fetch_array($audit8);
+
+        $audit9 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.2' AND id_statkerja='2'");
+        $bedit2 = mysqli_fetch_array($audit9);
+
+        $audit10 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.3' AND id_statkerja='2'");
+        $bedit3 = mysqli_fetch_array($audit10);
+
+        $audit11 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.4' AND id_statkerja='2'");
+        $bedit4 = mysqli_fetch_array($audit11);
+
+        $audit12 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.5' AND id_statkerja='2'");
+        $bedit5 = mysqli_fetch_array($audit12);
+
+        $audit13 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.6' AND id_statkerja='2'");
+        $bedit6 = mysqli_fetch_array($audit13);
+
+        $audit14 = mysqli_query($con, "SELECT COUNT(id_jab) AS jafung FROM pegawai WHERE id_jab='08.7' AND id_statkerja='2'");
+        $bedit7 = mysqli_fetch_array($audit14);
         ?>
     <script>
         $(function (){
@@ -162,7 +267,25 @@ if ($_SESSION['leveluser'] == '1' or $_SESSION['leveluser'] == '2') {
                         "Pranata Komputer Pertama",
                         "Pranata Komputer Muda",
                         "Pranata Komputer Madya",
-                        "Pranata Komputer Utama",],
+                        "Pranata Komputer Utama",
+                        "Pustakawan Pelaksana/Terampil",
+                        "Pustakawan Pelaksana Lanjutan/Mahir",
+                        "Pustakawan Penyelia",
+                        "Pustakawan Pertama",
+                        "Pustakawan Muda",
+                        "Pustakawan Madya",
+                        "Pustakawan Utama",
+                        "Perawat Pemula",
+                        "Perawat Pelaksana/Terampil",
+                        "Perawat Pelaksana Lanjutan/Mahir",
+                        "Perawat Penyelia",
+                        "Perawat Pertama",
+                        "Perawat Muda",
+                        "Perawat Madya",
+                        "Perawat Gigi Pemula",
+                        "Perawat Gigi Pelaksana/Terampil",
+                        "Perawat Gigi Pelaksana Lanjutan/Mahir",
+                        "Perawat Gigi Penyelia"],
                     title: {
                         text: null
                     }
@@ -204,37 +327,51 @@ if ($_SESSION['leveluser'] == '1' or $_SESSION['leveluser'] == '2') {
                 series:[{
                         name: 'Aktif',
                         data: [
-                            <?php echo $jafung['jafung']; ?>,
-                            <?php echo $jafung1['jafung']; ?>,
-                            <?php echo $jafung2['jafung']; ?>,
-                            <?php echo $jafung['jafung']; ?>,
-                            <?php echo $jafung1['jafung']; ?>,
-                            <?php echo $jafung2['jafung']; ?>,
-                            <?php echo $jafung['jafung']; ?>,
-                            <?php echo $jafung1['jafung']; ?>,
-                            <?php echo $jafung2['jafung']; ?>,
-                            <?php echo $jafung2['jafung']; ?>,
-                            <?php echo $jafung['jafung']; ?>,
-                            <?php echo $jafung1['jafung']; ?>,
-                            <?php echo $jafung2['jafung']; ?>
+                            <?php echo $ar1['jafung']; ?>,
+                            <?php echo $ar2['jafung']; ?>,
+                            <?php echo $ar3['jafung']; ?>,
+                            <?php echo $ar4['jafung']; ?>,
+                            <?php echo $ar5['jafung']; ?>,
+                            <?php echo $ar6['jafung']; ?>,
+                            <?php echo $ar7['jafung']; ?>,
+                            <?php echo $ankep1['jafung']; ?>,
+                            <?php echo $ankep2['jafung']; ?>,
+                            <?php echo $ankep3['jafung']; ?>,
+                            <?php echo $ankep4['jafung']; ?>,
+                            <?php echo $ankep5['jafung']; ?>,
+                            <?php echo $ankep6['jafung']; ?>,
+                            <?php echo $akdit1['jafung']; ?>,
+                            <?php echo $akdit2['jafung']; ?>,
+                            <?php echo $akdit3['jafung']; ?>,
+                            <?php echo $akdit4['jafung']; ?>,
+                            <?php echo $akdit5['jafung']; ?>,
+                            <?php echo $akdit6['jafung']; ?>,
+                            <?php echo $akdit7['jafung']; ?>
                         ],
                         color: '#FFB41A',
                 },{
                     name: 'Bebas Sementara',
                     data: [
-                        <?php echo $bebas['jafung']; ?>,
-                        <?php echo $bebas1['jafung']; ?>,
-                        <?php echo $bebas2['jafung']; ?>,
-                        <?php echo $bebas['jafung']; ?>,
-                        <?php echo $bebas1['jafung']; ?>,
-                        <?php echo $bebas2['jafung']; ?>,
-                        <?php echo $bebas['jafung']; ?>,
-                        <?php echo $bebas1['jafung']; ?>,
-                        <?php echo $bebas2['jafung']; ?>,
-                        <?php echo $bebas2['jafung']; ?>,
-                        <?php echo $bebas['jafung']; ?>,
-                        <?php echo $bebas1['jafung']; ?>,
-                        <?php echo $bebas2['jafung']; ?>
+                        <?php echo $arb1['jafung']; ?>,
+                        <?php echo $arb2['jafung']; ?>,
+                        <?php echo $arb3['jafung']; ?>,
+                        <?php echo $arb4['jafung']; ?>,
+                        <?php echo $arb5['jafung']; ?>,
+                        <?php echo $arb6['jafung']; ?>,
+                        <?php echo $arb7['jafung']; ?>,
+                        <?php echo $bkep1['jafung']; ?>,
+                        <?php echo $bkep2['jafung']; ?>,
+                        <?php echo $bkep3['jafung']; ?>,
+                        <?php echo $bkep4['jafung']; ?>,
+                        <?php echo $bkep5['jafung']; ?>,
+                        <?php echo $bkep6['jafung']; ?>,
+                        <?php echo $bedit1['jafung']; ?>,
+                        <?php echo $bedit2['jafung']; ?>,
+                        <?php echo $bedit3['jafung']; ?>,
+                        <?php echo $bedit4['jafung']; ?>,
+                        <?php echo $bedit5['jafung']; ?>,
+                        <?php echo $bedit6['jafung']; ?>,
+                        <?php echo $bedit7['jafung']; ?>
                     ],
                     color: 'orchid',
                 }]
@@ -279,53 +416,47 @@ if ($_SESSION['leveluser'] == '1' or $_SESSION['leveluser'] == '2') {
                   text: 'Sumber: Biro Organisasi dan Tata Laksana'  
                 },
                 xAxis: {
-                    categories: ["Arsiparis Pelaksana/Terampil",
-                        "Arsiparis Pelaksana Lanjutan/Mahir",
-                        "Arsiparis Penyelia",
-                        "Arsiparis Ahli Pertama",
-                        "Arsiparis Ahli Muda",
-                        "Arsiparis Ahli Madya",
-                        "Arsiparis Utama",
-                        "Analis Kepegawaian Pelaksana/Terampil",
-                        "Analis Kepegawaian Pelaksana Lanjutan/Mahir",
-                        "Analis Kepegawaian Penyelia", 
-                        "Analis Kepegawaian Pertama", 
-                        "Analis Kepegawaian Muda",
-                        "Analis Kepegawaian Madya",
-                        "Auditor Pelaksana/Terampil",
-                        "Auditor Pelaksana Lanjutan/Mahir",
-                        "Auditor Penyelia",
-                        "Auditor Pertama",
-                        "Auditor Muda",
-                        "Auditor Madya",
-                        "Auditor Utama",
-                        "Dokter Pertama",
-                        "Dokter Muda",
-                        "Dokter Madya",
-                        "Dokter Utama",
-                        "Dokter Gigi Pertama",
-                        "Dokter Gigi Muda",
-                        "Dokter Gigi Madya",
-                        "Dokter Gigi Utama",
-                        "Perancang Peraturan Perundang-Undangan Pertama",
-                        "Perancang Peraturan Perundang-Undangan Muda",
-                        "Perancang Peraturan Perundang-Undangan Madya",
-                        "Perancang Peraturan Perundang-Undangan Utama",
-                        "Pranata Humas Pemula",
-                        "Pranata Humas Pelaksana/Terampil",
-                        "Pranata Humas Pelaksana Lanjutan/Mahir",
-                        "Pranata Humas Penyelia",
-                        "Pranata Humas Pertama",
-                        "Pranata Humas Muda",
-                        "Pranata Humas Madya",
-                        "Pranata Komputer Pemula",
-                        "Pranata Komputer Pelaksana/Terampil",
-                        "Pranata Komputer Pelaksana Lanjutan/Mahir",
-                        "Pranata Komputer Penyelia",
-                        "Pranata Komputer Pertama",
-                        "Pranata Komputer Muda",
-                        "Pranata Komputer Madya",
-                        "Pranata Komputer Utama",],
+                    categories: [
+                        "Radiografer Pelaksana/Terampil",
+                        "Radiografer Pelaksana Lanjutan/Mahir",
+                        "Radiografer Penyelia",
+                        "Pranata Laboratorium Kesehatan Pemula",
+                        "Pranata Laboratorium Kesehatan Pelaksana/Terampil",
+                        "Pranata Laboratorium Kesehatan Pelaksana Lanjutan/Mahir",
+                        "Pranata Laboratorium Kesehatan Penyelia",
+                        "Pranata Laboratorium Kesehatan Pertama",
+                        "Pranata Laboratorium Kesehatan Muda",
+                        "Pranata Laboratorium Kesehatan Madya",
+                        "Bidan Pelaksana/Terampil",
+                        "Bidan Pelaksana Lanjutan/Mahir",
+                        "Bidan Penyelia",
+                        "Bidan Pertama",
+                        "Bidan Muda",
+                        "Bidan Madya",
+                        "Apoteker Pemula",
+                        "Apoteker Pelaksana/Terampil",
+                        "Apoteker Pelaksana Lanjutan/Mahir",
+                        "Apoteker Penyelia",
+                        "Penerjemah Pertama",
+                        "Penerjemah Muda",
+                        "Penerjemah Madya",
+                        "Penerjemah Utama",
+                        "Widyaiswara Pertama",
+                        "Widyaiswara Muda",
+                        "Widyaiswara Madya",
+                        "Widyaiswara Utama",
+                        "Auditor Kepegawaian Pertama",
+                        "Auditor Kepegawaian Muda",
+                        "Auditor Kepegawaian Madya",
+                        "Auditor Kepegawaian Utama",
+                        "Pengelola Pengadaan Barang/Jasa Pertama",
+                        "Pengelola Pengadaan Barang/Jasa Muda",
+                        "Pengelola Pengadaan Barang/Jasa Madya",
+                        "Pengelola Pengadaan Barang/Jasa Utama",
+                        "Pengawas Penyelenggaraan Urusan Pemerintah Di Daerah Pertama",
+                        "Pengawas Penyelenggaraan Urusan Pemerintah Di Daerah Muda",
+                        "Pengawas Penyelenggaraan Urusan Pemerintah Di Daerah Madya",
+                        "Pengawas Penyelenggaraan Urusan Pemerintah Di Daerah Utama"],
                     title: {
                         text: null
                     }
